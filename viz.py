d4321ba8af59cb8a4d6eb5e9226c9253b2fa7473
Fremont28 <johnmccool31@gmail.com>
2017-10-24 12:10:04 -0400

    Add files via upload
    Added a scatterplot to the dash visualization.

--- viz.py
+++ viz.py
@@ -1,5 +1,4 @@
 #Create dash visualizations with integer, floats, and string data types
-
 #import dash components 
 import dash
 import dash_core_components as dcc
@@ -8,7 +7,44 @@ import plotly.graph_objs as go
 import pandas as pd 
 import matplotlib.pyplot as plt
 
-#1. Integers
+# 1. Continuous data type (Scatterplot)
+app = dash.Dash()
+data1=pd.read_csv('bjpols_R2.csv') #read in the data set 
+
+#app layout for visualization 
+app.layout = html.Div([
+    dcc.Graph(
+        id='ln_cgdppc_L1 vs. growth_L1',
+        figure={
+            'data': [
+                go.Scatter(
+                    x=data1[data1['country'] == i]['ln_cgdppc_L1'],
+                    y=data1[data1['country'] == i]['growth_L1'],
+                    text=data1[data1['country'] == data1]['country'],
+                    mode='markers',
+                    opacity=0.7,
+                    marker={
+                        'size': 15,
+                        'line': {'width': 0.5, 'color': 'white'}
+                    },
+                    name=i
+                ) for i in data1.country.unique()
+            ],
+            'layout': go.Layout(
+                xaxis={'type': 'log', 'title': 'ln_cgdppc_L1'},
+                yaxis={'title': 'growth_L1'},
+                margin={'l': 40, 'b': 40, 't': 10, 'r': 10},
+                legend={'x': 0, 'y': 1},
+                hovermode='closest'
+            )
+        }
+    )
+])
+
+if __name__ == '__main__':
+    app.run_server()
+
+#2. Integers
 data1=pd.read_csv('FL-1990-births (1).csv')
 
 #create a bar graph
@@ -36,7 +72,7 @@ app.layout=html.H1(children=[
 if __name__ == '__main__':
     app.run_server(debug=True)
 
-#2. Floats (Histogram)*************
+#3. Floats (Histogram)*************
 data2=pd.read_csv('bjpols_R2.csv')
 
 import matplotlib.pyplot as plt 
@@ -49,6 +85,7 @@ sub_data2.hist()
 #test with dash (create histogram)*****
 trace2=go.Histogram(x='sppop_L1')
 
+app=dash.Dash() 
 app.layout=html.H1(children=[
     html.H1(children='SPPOP_L1'),
     dcc.Graph(
@@ -63,7 +100,7 @@ app.layout=html.H1(children=[
 if __name__ == '__main__':
     app.run_server(debug=True)
 
-#3. Categorical bar plot
+#4. Categorical bar plot
 
 #barplot for countries (counts)
 data2['country'].value_counts().plot(kind="bar")
@@ -83,6 +120,7 @@ app.layout=html.H1(children=[
 
 if __name__ == '__main__':
     app.run_server(debug=True)
+
 
 
 
